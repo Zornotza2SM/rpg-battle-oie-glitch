66c93f77811f3b2b9d5eba4f8be7794ee8af67a1
oie-glitch <o_eloseguiuri@fpzornotzalh.eus>
2025-12-09 18:34:23 +0100

    1. Maila OSATUTA: Game Over

--- rpg.js
+++ rpg.js
@@ -3,27 +3,44 @@ let vidaHeroe = 50;
 let vidaMonstruo = 50;
 // --- BISTA ---
 function vista() {
-    console.log("vista sartu da")
+     // Egoerak kalkulatu
+    let heroeMuerto = vidaHeroe <= 0;
+    let monstruoMuerto = vidaMonstruo <= 0;
+
+    // Emojiak erabaki (Ternarioa)
+    let iconoHeroe = heroeMuerto ? "⚰️" : "🥷";
+    let iconoMonstruo = monstruoMuerto ? "💥" : "👹";
+
+    
+ // Mezu bat prestatu
+    let mezua = "";
+    if (heroeMuerto) mezua = "<h3 style='color:red'>GARAITUA IZAN ZARA...</h3>";
+    if (monstruoMuerto) mezua = "<h3 style='color:gold'>IRABAZI DUZU! 🏆</h3>";
+
     document.getElementById("battle-app").innerHTML = `
         <div class="battle-screen">
             <h2>⚔️ BATTLE ARENA ⚔️</h2>
+             ${mezua}
             
             <div class="stats">
                 <div>HEROIA: ${vidaHeroe} HP</div>
                 <div>MUNSTROA: ${vidaMonstruo} HP</div>
             </div>
 
-            <div class="fighters">
-                <div>🥷</div>
-                <div>👹</div>
+          <div class="fighters">
+                <div>${iconoHeroe}</div>
+                <div>${iconoMonstruo}</div>
             </div>
+
 
             <div class="controls">
                 <button class="boton" id="btn-atacar">⚔️ Eraso</button>
                 <button class="boton heal" id="btn-curar">🧪 Sendatu</button>
             </div>
         </div>
+        
     `;
+
 
     // HEMEN JOANGO DIRA GERTAERAK (3. FASEA)
     // --- EGUNERATZEA (Gertaerak) ---
